545eb79b40c49d0a76f01ed90875480cb65e9c0d
w00suk1234 <w00suk1234@naver.com>
2020-10-07 16:22:42 +0900

    Initial Commit

---
+++ GitTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest1
+//
+//  Created by 이우석 on 2020/10/07.
+//  Copyright © 2020 com.woosuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
